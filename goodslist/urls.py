843c167c9a9fd5ddb6a696f22a2ba2ca6539599b
yzl123456 <376712116@qq.com>
2017-10-16 22:46:57 +0800

    登陆注册 商品列表 页面显示

--- goodslist/urls.py
+++ goodslist/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    #url(r'^list/$', views.list, name='list'),
+    url(r'^list/$', views.list, name='list'),
   
 ]
  # url(r'^list/$',views.list,name='list'),
