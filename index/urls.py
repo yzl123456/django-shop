4dd6936f713de4f9066a64174ded62b9cc68c07e
yzl123456 <376712116@qq.com>
2017-11-30 18:11:35 +0800

    完善配置

--- index/urls.py
+++ index/urls.py
@@ -5,11 +5,8 @@ from . import views
 urlpatterns = [
 	# url(r'^/$',views.index,name='indexPage2'),
     url(r'^index/$',views.index,name='indexPage'),
-    url(r'^aboutus/$',views.aboutus,name='aboutus'),
-    url(r'^callus/$',views.callus,name='callus'),
-    url(r'^joinus/$',views.joinus,name='joinus'),
+
  	url(r'^loginOut/$',views.loginOut,name='loginOut'),
- 	url(r'^choujiang/$',views.choujiang,name='choujiang'),
 
     ]
 
--- shop/urls.py
+++ shop/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from index import viewSearch
-from haystack.views import SearchView
 from index.views import search
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
